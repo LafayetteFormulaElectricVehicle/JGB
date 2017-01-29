4f252b58c0e334488f6bead7467a9f34c523cdc4
Gregf36665 <Flynng@lafayette.edu>
2017-01-28 20:12:16 -0500

    Updated main to test functions in a more tidy method

--- software/src/main.c
+++ software/src/main.c
@@ -10,9 +10,7 @@
 
 #include <stdio.h>
 
-#include "can_lib.h"
 #include "jgb.h"
-#include "timer.h"
 #include "uart.h"
 
 //Device CAN_ID
@@ -21,19 +19,13 @@
 
 #define UART_NO_DATA 0x100
 
-//uint8_t response_data[CAN_BUFFER_SIZE];
-uint8_t can_buffer_tx[CAN_BUFFER_SIZE];
-uint8_t can_buffer_rx[CAN_BUFFER_SIZE];
-
 // Buffer for UART message
 char buffer[30];
 
 void setup(void) {
 	analog_setup(ADC_AVCC, ADC_SCALE_128);
-
+	dac_enable();
 	uart_init(BAUD);
-	
-	can_init(0);
 	
 	fdevopen(&uart_putc, NULL);
 	fdevopen(&uart_getc, NULL);
@@ -41,34 +33,38 @@ void setup(void) {
 	sei();
 }
 
+void adc_test(void){
+		sprintf(buffer, "ADC 1 reads %i",adc_read(ADC1)); 
+		puts(buffer);
+		sprintf(buffer, "ADC 2 reads %i",adc_read(ADC2)); 
+		puts(buffer);
+		sprintf(buffer, "ADC 3 reads %i",adc_read(ADC3)); 
+		puts(buffer);
+		sprintf(buffer, "ADC 4 reads %i",adc_read(ADC4)); 
+		puts(buffer);
+		_delay_ms(500);
+
+}
+
+void dac_test(void){
+	dac_write(0x3FF);
+	puts("Set DAC to 2^10-1 (1023)");
+	strcpy(buffer, "Set DAC to 0x40");
+	puts(buffer);
+	dac_write(0x40);
+	_delay_ms(500);
+	strcpy(buffer, "Set DAC to 0x3FF");
+	puts(buffer);
+	dac_write(0x3FF);
+
+}
+
 int main(void) {
-	st_cmd_t tx_msg;
-	st_cmd_t rx_msg;
 	//Setup Comment
 	setup();
 
-	uint16_t val = 0x0000;
-	char str [] = "Enter DAC count (0-1023)";
-	int buffer_index = 0;
-	puts("Enabling DAC");
-	dac_enable();
-	puts("DAC Online");
-	dac_write(0x3FF);
-	puts("Set DAC to 2^10-1 (1023)");
-	for(;;) {
-		strcpy(str, "Set DAC to 0x40");
-		puts(str);
-		dac_write(0x40);
-		sprintf(buffer, "ADC 1 reads %i",adc_read(ADC3)); 
-		puts(buffer);
-		_delay_ms(500);
-
-		strcpy(str, "Set DAC to 0x3FF");
-		sprintf(buffer, "ADC 1 reads %i",adc_read(ADC3)); 
-		puts(buffer);
-		dac_write(0x3FF);
-
-		_delay_ms(500);
+	while(1){
+		adc_test();
 	}
 	
 	return 0;
